e7958ea2ce8c7715f104603ecb55edd22a794998
ZUnead Ahmed <zuned1996@outlook.com>
2021-09-19 21:28:04 +0600

    updated

--- src/pages/Home-page/HomePage.js
+++ src/pages/Home-page/HomePage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Homepage = () => {
-    
+const HomePage = () => {
     return (
         <div>
-
+            
         </div>
     );
 };
 
-export default Homepage;
+export default HomePage;
