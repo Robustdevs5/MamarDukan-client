611bbeca7898b354f63e1e49f24f5b1c28582a3f
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-09-21 14:28:13 +0600

    arrow modified

--- src/Components/FeaturedProduct/FeaturedProduct.js
+++ src/Components/FeaturedProduct/FeaturedProduct.js
@@ -4,18 +4,51 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import '../StyledComponent/HomepageProduct.css';
 import star from "../../images/5star.png";
+import right from "../../images/right.png";
+import left from "../../images/left.png";
 
 
 
 const FeaturedProduct = () => {
 
+    function SampleNextArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick} >
+
+                <img className="oi"
+                    src={right} alt="" />
+
+            </div>
+        );
+    }
+
+    function SamplePrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick} >
+
+                <img className="oi"
+                    src={left} alt="" />
+
+            </div>
+        );
+    }
+
+
     var settings = {
         dots: false,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 5,
         slidesToScroll: 4,
         initialSlide: 0,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
         responsive: [
             {
                 breakpoint: 1024,
@@ -49,95 +82,95 @@ const FeaturedProduct = () => {
     return (
         <div className="p-2">
             <h1 className="text-3xl text-gray-800 font-bold mb-1 py-7 px-4">Featured Product</h1>
-            
-            <Slider {...settings} className="p-1">
+
+            <Slider {...settings} className="px-12">
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://i.ibb.co/dGxSgHk/8192.jpg" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1622979138084-c03ae28968ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1515849430397-7aee921bbea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://i.ibb.co/dGxSgHk/8192.jpg" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1622979138084-c03ae28968ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1515849430397-7aee921bbea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://i.ibb.co/dGxSgHk/8192.jpg" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1622979138084-c03ae28968ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1515849430397-7aee921bbea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://i.ibb.co/dGxSgHk/8192.jpg" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1622979138084-c03ae28968ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1515849430397-7aee921bbea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
                     <h5 className="text-2xl font-bold text-green-700">$100</h5>
                     <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{width: '100px', height: '25px'}} alt="" />
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
                     <small>Sold: (150)</small>
                 </div>
-               
+
             </Slider>
-            
+
         </div>
     );
 };
--- src/Components/MostViewedProducts/MostViewedProducts.js
+++ src/Components/MostViewedProducts/MostViewedProducts.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import Slider from "react-slick";
-// import '../StyledComponent/HomepageProduct.css';
 import star from "../../images/5star.png";
+import right from "../../images/right.png";
+import left from "../../images/left.png";
 
 
 
 const MostViewedProducts = () => {
+
+    function SampleNextArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick} >
+
+                <img className="oi"
+                    src={right} alt="" />
+
+            </div>
+        );
+    }
+
+    function SamplePrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick} >
+
+                <img className="oi"
+                    src={left} alt="" />
+
+            </div>
+        );
+    }
+
 
     const settings = {
         className: "center",
@@ -19,6 +49,8 @@ const MostViewedProducts = () => {
         autoplaySpeed: 3000,
         autoplay: true,
         pauseOnHover: true,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
 
         responsive: [
             {
@@ -33,7 +65,7 @@ const MostViewedProducts = () => {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 2,
                     initialSlide: 2
                 }
@@ -52,9 +84,66 @@ const MostViewedProducts = () => {
 
     return (
         <div className="p-2 py-5 my-9">
-            <h1 className="text-3xl text-gray-800 font-bold py-7 px-4">Top Categories</h1>
+            <h1 className="text-3xl text-gray-800 font-bold py-7 px-4">Most viewed Products</h1>
 
-            <Slider {...settings}>
+            <Slider {...settings} className="px-10">
+
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1515849430397-7aee921bbea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://i.ibb.co/dGxSgHk/8192.jpg" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1622979138084-c03ae28968ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1566150905458-1bf1fc113f0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1596273501899-336404ed1701?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1587829741301-dc798b83add3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
+                <div className="p-1 pl-2 pr-2">
+                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1589401806207-2381455bce76?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
+                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
+                    <h3 className="text-blue-700">Product 101</h3>
+                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
+                    <small>Sold: (150)</small>
+                </div>
 
                 <div className="p-1 pl-2 pr-2">
                     <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1515849430397-7aee921bbea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
@@ -113,64 +202,7 @@ const MostViewedProducts = () => {
                     <small>Sold: (150)</small>
                 </div>
 
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1515849430397-7aee921bbea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://i.ibb.co/dGxSgHk/8192.jpg" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1622979138084-c03ae28968ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1566150905458-1bf1fc113f0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1596273501899-336404ed1701?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1587829741301-dc798b83add3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                <div className="p-1 pl-2 pr-2">
-                    <img className="mb-4 rounded cursor-pointer" src="https://images.unsplash.com/photo-1589401806207-2381455bce76?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="8192" />
-                    <h5 className="text-2xl font-bold text-green-700">$100</h5>
-                    <h3 className="text-blue-700">Product 101</h3>
-                    <img src={star} style={{ width: '100px', height: '25px' }} alt="" />
-                    <small>Sold: (150)</small>
-                </div>
-                
-                
+
 
             </Slider>
         </div>
