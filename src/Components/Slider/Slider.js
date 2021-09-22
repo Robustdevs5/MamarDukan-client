e9b7de1c56bc1b7459545eb649bd6e4dc29f06d5
sahadat <shmanik14@yahoo.com>
2021-09-22 11:56:51 +0600

    slider final

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -2,31 +2,35 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import slider1 from '../../Images/slider-1.jpg';
-import slider2 from '../../Images/slider-2.jpg';
-import slider3 from '../../Images/slider-3.jpg';
+import p1 from '../../images/promotion-1.jpg';
+import p2 from '../../images/promotion-2.jpg';
+import slider1 from '../../images/slider-11.jpg';
+import slider2 from '../../images/slider-12.jpg';
+import slider3 from '../../images/slider-13.jpg';
 import '../StyledComponent/Global.css';
 
 const Sliders = () => {
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 1000,
         slidesToShow: 1,
         autoplay: true,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        fade: true
     };
     return (
-        <div className="container mx-auto slider">
+        <div className="mx-4 xs:grid-cols-1 md:grid grid-cols-3 gap-4 slider pt-2">
             <div class="md:col-span-2 slider-blog">
-                <Slider {...settings}>
+            <Slider {...settings}>
                     <div className="sin-slider-blog cursor-pointer">
                         <div className="slider-blog-img">                   
                             <img src={slider1} alt="Slider Avatar"/>
                         </div>
                         <div className="slider-blog-content">
-                            <h2>Hello World</h2>
+                            <h2>Welcome To Mamur Dukhan</h2>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, deserunt!</p>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full">Shop Now</button>
                         </div>
                     </div>
                     <div className="sin-slider-blog cursor-pointer">
@@ -34,8 +38,9 @@ const Sliders = () => {
                             <img src={slider2} alt="Slider Avatar"/>
                         </div>
                         <div className="slider-blog-content">
-                            <h2>Hello World</h2>
+                            <h2>Welcome To Mamur Dukhan</h2>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, deserunt!</p>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full">Shop Now</button>
                         </div>
                     </div>
                     <div className="sin-slider-blog cursor-pointer">
@@ -43,11 +48,20 @@ const Sliders = () => {
                             <img src={slider3} alt="Slider Avatar"/>
                         </div>
                         <div className="slider-blog-content">
-                            <h2>Hello World</h2>
+                            <h2>Welcome To Mamur Dukhan</h2>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, deserunt!</p>
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full">Shop Now</button>
                         </div>
                     </div>
                 </Slider>
+            </div>
+            <div className="promotion-post">
+                <div className="pb-2">
+                    <a href="#"><img src={p1} alt="Promotion banner" /></a>
+                </div>
+                <div>
+                    <a href="#"><img src={p2} alt="Promotion banner" /></a>
+                </div>
             </div>
         </div>
     );
