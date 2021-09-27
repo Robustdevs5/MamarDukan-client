e19926df9514c0313159ef6923acb0a2b51f13ee
Zuned Ahmed <63390198+zunead@users.noreply.github.com>
2021-09-27 17:37:24 +0600

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <AdminPanel />
         </Route>
         <Route path="/addProduct">
-          <AddProduct />
+          {/* <AddProduct /> */}
         </Route>
         <Route path="/manageProduct">
           <ManageProduct />
