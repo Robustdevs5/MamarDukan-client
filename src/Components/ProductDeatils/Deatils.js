a86fce97223ce2daf5bc1b47a682e0eefede0e9a
Tamal <tamal7971@gmail.com>
2021-09-27 11:19:15 +0600

    Working on Product page(Tamal)

--- src/Components/ProductDeatils/Deatils.js
+++ src/Components/ProductDeatils/Deatils.js
@@ -6,11 +6,12 @@ const Deatils = () => {
     return (
         <div className="Details">
            <div className="right">
-                <img src={img}></img>
+                <img className="m-auto" src={img}></img>
            </div>
            <div className="left">
                <h1>Title of Deatils Dukan amet consectetur adipisicing elit</h1> 
                 <p>spacefication</p>
+                <p>Brand : Tamal </p>
                 <label>Choose a size</label>
                 <select name="Size">
                     <option value="S">S</option>
@@ -19,9 +20,18 @@ const Deatils = () => {
                     <option value="XL">XL</option>
                     <option value="XXL">XXL</option>
                 </select>
+                <br/>
+                <label>Choose a Color</label>
+                <select name="Color">
+                    <option value="Red">Red</option>
+                    <option value="Green">Green</option>
+                    <option value="Yellow">Yellow</option>
+                    <option value="Red">Red</option>
+                    <option value="White">White</option>
+                </select>
                 <h3>Price: 500$</h3>
-                <div style={{display: 'flex'}} className="seller">
-                    <img className="w-12 mr-6" src={img}></img>
+                <div style={{display: 'flex', border: '1px solid black'}} className="seller">
+                    <img className="w-12 mr-6 " src={img}></img>
                     <p>seller</p>
                 </div>
                 <button>Add to Cart</button>
