f2f7b52a50e4e45917065454945c407a6366b11a
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-02 11:57:31 +0600

    pulling

--- src/Components/AdminDashboard/AddProduct/AddProduct.js
+++ src/Components/AdminDashboard/AddProduct/AddProduct.js
@@ -236,7 +236,7 @@ const AddProduct = () => {
                                         required
                                     />
                                 </div>
-
+                                
                                 <div class="mb-6">
                                     <label
                                         class="block text-gray-700 text-sm font-bold mb-2"
