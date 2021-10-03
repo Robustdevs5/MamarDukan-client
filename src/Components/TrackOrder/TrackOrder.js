a0240252d5293eeb132d9229f5bb5a4f34d588c1
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-03 18:48:30 +0600

    order tracking page done

--- src/Components/TrackOrder/TrackOrder.js
+++ src/Components/TrackOrder/TrackOrder.js
@@ -4,11 +4,45 @@ import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar/Navbar';
 import TopBar from '../TopBar/TopBar';
 
+
+
 const TrackOrder = () => {
 
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
 
+        const orderInfo = {
+            // orderId: e.target.orderId.value,
+            // email: e.target.email.value
+        };
+
+        const userSignUp = `http://localhost:5000/`;
+        fetch(userSignUp, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(orderInfo)
+        })
+            .then(async res => await res.json())
+            .then(async user => {
+                console.log('user10', user)
+                // user ? alert(user.message) : alert("failed")
+                //     if (user) {
+                //         toast.success(user.message, {
+                //             position: "bottom-right",
+                //         });
+                //     }
+                //     sessionStorage.setItem('user', JSON.stringify(user));
+                //     history.push('/')
+                // })
+                // .catch(error => {
+                // alert(error.message);
+                // console.log(error);
+                //     toast.error(error.message, {
+                //         position: "bottom-right",
+                //     });
+            });
     }
 
 
@@ -21,7 +55,7 @@ const TrackOrder = () => {
             <div className="form-card">
                 <form onSubmit={handleSubmit()}>
 
-                    <div className="pb-12">
+                    <div className="pb-10">
                         <h3 className="text-2xl text-gray-800">Order tracking</h3>
                         <p className="text-sm py-1 text-gray-500">Tracking your order status</p>
                     </div>
@@ -36,6 +70,7 @@ const TrackOrder = () => {
 
                 </form>
             </div>
+
             <ToastContainer />
             <Footer />
         </>
