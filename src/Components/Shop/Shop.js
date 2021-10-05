8edea088ce447ce4eff720fac219f7f82a623a7f
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-05 15:43:37 +0600

    some updates in shop

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,9 +1,7 @@
-import { TablePagination } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import TopBar from "../../Components/TopBar/TopBar";
 import Navbar from "../../Components/Navbar/Navbar/Navbar";
-import star from "../../images/5star.png";
-import { useHistory } from 'react-router';
+import AllProduct from './AllProduct';
 // import noUiSlider from 'nouislider';
 // import noUiSlider from 'nouislider/dist/nouislider.mjs';
 // import * as noUiSlider from 'nouislider';
@@ -13,8 +11,6 @@ import { useHistory } from 'react-router';
 const Shop = () => {
 
     const [product, setProduct] = useState([]);
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(12);
 
     //Fetching Product.............................................
     useEffect(() => {
@@ -23,17 +19,6 @@ const Shop = () => {
             .then(data => setProduct(data.products))
     }, [])
     console.log(product);
-
-
-    // pagination........................................................
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(event.target.value);
-        setPage(0);
-    };
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
 
 
     //Department unique Names
@@ -60,23 +45,11 @@ const Shop = () => {
     console.log(uniqBrandName)
 
 
-
-    const history = useHistory();
-    const handleProductClick = (id) => {
-        history.push(`/product/${id}`);
+    const handleDepartmentClick = (dept) => {
+        console.log(dept);
     }
 
-    // var slider = document.querySelector('.slider')
-    //var slider = document.getElementById('slider');
-    // noUiSlider.create(slider, {
-    //     start: [20, 80],
-    //     connect: true,
-    //     range: {
-    //         'min': 0,
-    //         'max': 100
-    //     }
-    // });
-
+    
 
 
     return (
@@ -93,7 +66,12 @@ const Shop = () => {
                         <h1 className="text-lg mb-5">PRODUCT DEPARTMENT</h1>
                         {
                             uniqDeptName.map(uniqDeptName =>
-                                <p className="cursor-pointer my-2">{uniqDeptName}</p>
+                                <p
+                                    className="cursor-pointer my-2"
+                                    onClick={() => handleDepartmentClick(uniqDeptName)}
+                                >
+                                    {uniqDeptName}
+                                </p>
                             )}
                     </div>
 
@@ -121,13 +99,6 @@ const Shop = () => {
                     <div className="px-5 mt-8 py-5 bg-blue-100 text-gray-800 rounded">
                         <h1 className="text-lg mb-5">BY PRICE</h1>
                         <input type="range" name="" id="" min="0" Max="100000" />
-
-                        {/* <div id="slider">
-                        </div> */}
-                        {/* <noUiSlider
-                            range={{ min: 0, max: 100 }}
-                            start={[20, 80]}
-                            connect /> */}
                     </div>
 
 
@@ -205,54 +176,13 @@ const Shop = () => {
 
 
                 <div className="px-10 pt-10">
-                    <div className="grid gap-12 grid-cols-4">
-                        {
-                            product
-                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map(product =>
-                                    <div className="p-2">
-
-                                        <div className="mb-4 w-44 h-44">
-                                            <img onClick={() => handleProductClick(product._id)} className="rounded cursor-pointer h-full w-full" src={product.img} alt="8192" />
-                                        </div>
-
-                                        <div className="flex py-3">
-                                            <h5 className="text-base font-bold text-green-700">${product.price}</h5>
-                                            <del className="px-4 text-base text-gray-500">10000</del>
-                                        </div>
-
-                                        <p className="text-gray-700 text-sm">Sold by: <span className="hover:text-blue-500 cursor-pointer"> Mr. Rahim</span></p>
-                                        <hr />
-
-                                        <div className="py-3">
-                                            <p onClick={() => handleProductClick(product._id)} className="text-blue-500 hover:text-yellow-500 cursor-pointer text-sm">{product.name}</p>
-
-                                            <div className="flex">
-                                                <img src={star} style={{ width: '60px', height: '15px' }} alt="" />
-                                                <p className="text-gray-600 text-xs px-1">(0)</p>
-                                            </div>
-
-                                            <p className="text-gray-600 text-xs px-1">Sold: 10</p>
-                                        </div>
-                                    </div>
-                                )}
-                    </div>
+                    <AllProduct />
                 </div>
 
 
             </div>
 
-            <TablePagination
-                className="mx-16"
-                rowsPerPageOptions={[]}
-                component="div"
-                count={product.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-                checkboxSelection
-            />
+
         </>
     );
 };
