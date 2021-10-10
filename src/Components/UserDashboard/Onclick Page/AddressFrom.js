f2fcdf97c008fee98f034db1607ffc95d177c8cb
Robustdevs5 <jahidhasananik.official@gmail.com>
2021-10-10 20:55:58 +0600

    checkboxt problem

--- src/Components/UserDashboard/Onclick Page/AddressFrom.js
+++ src/Components/UserDashboard/Onclick Page/AddressFrom.js
@@ -10,6 +10,9 @@ const AddressFrom = () => {
         e.preventDefault();
     }
 
+    const handleDefaultChange = (e) => {
+    }
+
 
 
     return (
@@ -57,28 +60,8 @@ const AddressFrom = () => {
                     className="border rounded border-gray-400 mb-6 w-full p-3"
                 />
 
-                {/* <input type="checkbox" id="def" name="defaultAddress" value="1"
-                    className="w-4 h-4"
-                />
-                <label for="def"
-                    className="mx-2 py-6"
-                >
-                    Use this as default
-                </label> */}
-
-                {/* <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" className="w-4 h-4"/> */}
-
-                {/* <input type="checkbox" name="asdas" id="vehicle1" value="ok"/>
-                <label for="vehicle1"> I have a bike</label> */}
-
-                {/* <input type="checkbox" name="defaultAddress" value="default" required
-                    className="w-4 h-4"
-                />
-                <label for="defaultAddress"> I have a bike</label> */}
-
-                {/* <input type='checkbox' name='thing' value='valuable' id="thing"/><label for="thing">dgdfgdg</label> <label>get an army</label> */}
-
-                <input type="checkbox" class="w-4 h-4 appearance-none checked:bg-blue-600 bg-gray-500 checked:border-transparent"/>
+                {/* <input type="radio" name="" id="" />
+                <level>dgdg</level> */}
 
                 <input type="submit" value="Submit"
                     className="p-3 primary_BTN_Outline rounded duration-300 w-full cursor-pointer"
