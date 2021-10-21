f8c5d42f12945c20a6303de2dadcea98285401d5
sahadat <shmanik14@yahoo.com>
2021-10-21 10:12:04 +0600

    [vendor] message

--- src/Components/VendorDashboard/VendorMessage/VendorMessage.js
+++ src/Components/VendorDashboard/VendorMessage/VendorMessage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import VendorSidebar from '../VendorSidebar/VendorSidebar';
 import VendorNavbar from '../VendorTopbar/VendorNavbar';
+import MessageOverview from './MessageOverview';
 
 const VendorMessage = () => {
     return (
@@ -8,6 +9,7 @@ const VendorMessage = () => {
                 <VendorSidebar />
             <div className="container mx-auto md:w-4/5 w-11/12">
                 <VendorNavbar />
+                <MessageOverview />
             </div>
         </div>
     );
