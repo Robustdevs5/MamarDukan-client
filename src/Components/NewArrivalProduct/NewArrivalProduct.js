aea1f6ff0dea052d71578f9752ca742a545c1af0
sahadat <shmanik14@yahoo.com>
2021-10-28 14:07:15 +0600

    recent product

--- src/Components/NewArrivalProduct/NewArrivalProduct.js
+++ src/Components/NewArrivalProduct/NewArrivalProduct.js
@@ -22,11 +22,11 @@ const NewArrivalProduct = () => {
                 <div className="mt-4 pl-2 mb-2 flex justify-between ">
                     <div>
                     <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
-                    <p className="text-md text-red-900 mt-0">$340</p>
+                    <p className="font-bold text-red-900 mt-0">$340</p>
                     </div>
                 </div>
-                <div>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
                 </div>                
             </div>
             <div className="relative w-full bg-white border cursor-pointer">
@@ -47,24 +47,8 @@ const NewArrivalProduct = () => {
                     <p className="text-md text-red-900 mt-0">$340</p>
                     </div>
                 </div>
-            </div>
-            <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
-                <div className="overflow-x-hidden relative border-b p-2">
-                    <img className="h-48 w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg" />
-                    <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                    </p>
-                    <div className="text-sm absolute top-2 left-2 bg-custom px-4 py-2 text-white rounded flex flex-col items-center justify-center hover:bg-white hover:text-red-600 transition duration-500 ease-in-out">
-                        <span className="font-bold">Sale</span>
-                    </div>
-                </div>
-                <div className="mt-4 pl-2 mb-2 flex justify-between ">
-                    <div>
-                    <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
-                    <p className="text-md text-red-900 mt-0">$340</p>
-                    </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
                 </div>
             </div>
             <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
@@ -84,6 +68,31 @@ const NewArrivalProduct = () => {
                     <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
                     <p className="text-md text-red-900 mt-0">$340</p>
                     </div>
+                </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
+                </div>
+            </div>
+            <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
+                <div className="overflow-x-hidden relative border-b p-2">
+                    <img className="h-48 w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg" />
+                    <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                    </p>
+                    <div className="text-sm absolute top-2 left-2 bg-custom px-4 py-2 text-white rounded flex flex-col items-center justify-center hover:bg-white hover:text-red-600 transition duration-500 ease-in-out">
+                        <span className="font-bold">Sale</span>
+                    </div>
+                </div>
+                <div className="mt-4 pl-2 mb-2 flex justify-between ">
+                    <div>
+                    <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
+                    <p className="text-md text-red-900 mt-0">$340</p>
+                    </div>
+                </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
                 </div>
             </div>
             <div className="relative w-full bg-white border cursor-pointer row-span-2 col-span-2">
@@ -104,24 +113,8 @@ const NewArrivalProduct = () => {
                     <p className="text-md text-red-900 mt-0">$340</p>
                     </div>
                 </div>
-            </div>
-            <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
-                <div className="overflow-x-hidden relative border-b p-2">
-                    <img className="h-48 w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg" />
-                    <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                    </p>
-                    <div className="text-sm absolute top-2 left-2 bg-custom px-4 py-2 text-white rounded flex flex-col items-center justify-center hover:bg-white hover:text-red-600 transition duration-500 ease-in-out">
-                        <span className="font-bold">Sale</span>
-                    </div>
-                </div>
-                <div className="mt-4 pl-2 mb-2 flex justify-between ">
-                    <div>
-                    <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
-                    <p className="text-md text-red-900 mt-0">$340</p>
-                    </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
                 </div>
             </div>
             <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
@@ -142,24 +135,8 @@ const NewArrivalProduct = () => {
                     <p className="text-md text-red-900 mt-0">$340</p>
                     </div>
                 </div>
-            </div>
-            <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
-                <div className="overflow-x-hidden relative border-b p-2">
-                    <img className="h-48 w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg" />
-                    <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                    </p>
-                    <div className="text-sm absolute top-2 left-2 bg-custom px-4 py-2 text-white rounded flex flex-col items-center justify-center hover:bg-white hover:text-red-600 transition duration-500 ease-in-out">
-                        <span className="font-bold">Sale</span>
-                    </div>
-                </div>
-                <div className="mt-4 pl-2 mb-2 flex justify-between ">
-                    <div>
-                    <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
-                    <p className="text-md text-red-900 mt-0">$340</p>
-                    </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
                 </div>
             </div>
             <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
@@ -179,6 +156,53 @@ const NewArrivalProduct = () => {
                     <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
                     <p className="text-md text-red-900 mt-0">$340</p>
                     </div>
+                </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
+                </div>
+            </div>
+            <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
+                <div className="overflow-x-hidden relative border-b p-2">
+                    <img className="h-48 w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg" />
+                    <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                    </p>
+                    <div className="text-sm absolute top-2 left-2 bg-custom px-4 py-2 text-white rounded flex flex-col items-center justify-center hover:bg-white hover:text-red-600 transition duration-500 ease-in-out">
+                        <span className="font-bold">Sale</span>
+                    </div>
+                </div>
+                <div className="mt-4 pl-2 mb-2 flex justify-between ">
+                    <div>
+                    <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
+                    <p className="text-md text-red-900 mt-0">$340</p>
+                    </div>
+                </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
+                </div>
+            </div>
+            <div className="relative max-w-sm min-w-[340px] bg-white border cursor-pointer">
+                <div className="overflow-x-hidden relative border-b p-2">
+                    <img className="h-48 w-full object-cover" src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg" />
+                    <p className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 group-hover:opacity-70" fill="none" viewBox="0 0 24 24" stroke="gray">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                    </p>
+                    <div className="text-sm absolute top-2 left-2 bg-custom px-4 py-2 text-white rounded flex flex-col items-center justify-center hover:bg-white hover:text-red-600 transition duration-500 ease-in-out">
+                        <span className="font-bold">Sale</span>
+                    </div>
+                </div>
+                <div className="mt-4 pl-2 mb-2 flex justify-between ">
+                    <div>
+                    <p className="text-lg font-semibold text-gray-900 mb-0">Russell Hobbs Kettle Premium Stainlessteel</p>
+                    <p className="text-md text-red-900 mt-0">$340</p>
+                    </div>
+                </div>
+                <div className="px-2 py-4">
+                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
                 </div>
             </div>
         </div>
