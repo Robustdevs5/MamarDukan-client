46cc8742600ee23fa1d9525a5ea35fe489b4a89e
sahadat <shmanik14@yahoo.com>
2021-10-28 11:56:45 +0600

    recent product

--- src/pages/Home-page/HomePage.js
+++ src/pages/Home-page/HomePage.js
@@ -1,17 +1,18 @@
 import React from "react";
+import AllInOne from "../../Components/AllInOne/AllInOne";
+import ClassicProducts from "../../Components/ClassicProducts/ClassicProducts";
 import FeaturedProduct from "../../Components/FeaturedProduct/FeaturedProduct";
+import Footer from "../../Components/Footer/Footer";
 import MostViewedProducts from "../../Components/MostViewedProducts/MostViewedProducts";
 import Navbar from "../../Components/Navbar/Navbar/Navbar";
+import NewArrivalProduct from "../../Components/NewArrivalProduct/NewArrivalProduct";
 import NewProducts from "../../Components/NewProducts/NewProducts";
-import Slider from "../../Components/Slider/Slider";
-import TopBar from "../../Components/TopBar/TopBar";
-import Footer from "../../Components/Footer/Footer";
-import { Policies } from "../../Components/Policies/Policies";
-import TechnologyProducts from "../../Components/TechnologyProducts/TechnologyProducts";
-import AllInOne from "../../Components/AllInOne/AllInOne";
 import Newsletter from "../../Components/Newsletter/Newsletter";
+import { Policies } from "../../Components/Policies/Policies";
+import Slider from "../../Components/Slider/Slider";
 import SummerStyles from "../../Components/SummerStyles/SummerStyles";
-import ClassicProducts from "../../Components/ClassicProducts/ClassicProducts";
+import TechnologyProducts from "../../Components/TechnologyProducts/TechnologyProducts";
+import TopBar from "../../Components/TopBar/TopBar";
 
 const HomePage = () => {
   return (
@@ -22,6 +23,7 @@ const HomePage = () => {
       <Policies />
       <NewProducts />
       <FeaturedProduct />
+      <NewArrivalProduct />
       <SummerStyles/>
       <ClassicProducts/>
       <TechnologyProducts />
