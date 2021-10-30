c1d9c5d3d0a0e79b7120f08be865b9ed41d92215
Tamal <tamal7971@gmail.com>
2021-10-30 14:32:05 +0600

    User Section  Done

--- src/Components/SuperAdminDashboard/SuperAdminManageUser/SuperAdmin-SuperAdminDashboard/SuperAdmin.js
+++ src/Components/SuperAdminDashboard/SuperAdminManageUser/SuperAdmin-SuperAdminDashboard/SuperAdmin.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import SuperAdminSidebar from '../../SuperAdminSidebar/SuperAdminSidebar';
-
+import DataTable from '../DataTable';
+import {SuperAdminData} from '../TableData';
 const SuperAdmin = () => {
     return (
-        <div className=" bg-gray-800 ">
-        {/* <div className="w-screen h-20 p-6">
-            <Logo />
-        </div> */}
-
+    <div className=" bg-gray-800 ">
         <div className="flex flex-wrap">
             <SuperAdminSidebar/>
-
-            <div className="sm:w-3/5 w-screen mx-4">
-                <h1 className="mt-3 text-2xl font-bold text-white m-2 ">
-                   Super Admin
-                </h1>
+            <div className="sm:w-4/5 w-screen mx-auto">
+                <h1 className="mt-3 text-2xl font-bold text-white m-2 ">All Super Admin Details</h1>
+                <DataTable Data={SuperAdminData} />
             </div>
         </div>
     </div>
---
+++ src/Components/SuperAdminDashboard/SuperAdminManageUser/TableData.js
@@ -0,0 +1,132 @@
+export const User = [
+    {
+        id : 1,
+        Name : 'User',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },
+    {
+        id : 2,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 3,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 4,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },
+
+]
+
+export const Vendor = [
+    {
+        id : 1,
+        Name : 'Vendor',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 2,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 3,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 4,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },
+
+]
+
+export const Admin = [
+    {
+        id : 1,
+        Name : 'Admin',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 2,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 3,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 4,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },
+
+]
+
+export const SuperAdminData = [
+    {
+        id : 1,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 2,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 3,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },{
+        id : 4,
+        Name : 'John',
+        Mail : 'John@example.com',
+        Phone : '123-456',
+        Status : 'Atcive',
+        Active : "Edit | Delete"
+    },
+
+]
