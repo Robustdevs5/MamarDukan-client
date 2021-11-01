4bbcbf79bac64b68416fd5425980f097daa0c6bd
sahadat <shmanik14@yahoo.com>
2021-11-01 11:44:47 +0600

    banner home

--- src/Components/HomePageBanner/HomePageBanner.js
+++ src/Components/HomePageBanner/HomePageBanner.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import bannerImg from '../../images/banner-0.png';
+
 const HomePageBanner = () => {
     return (
-        <div className="my-16" style={{backgroundColor: "#dd2400"}}>
-            <div class="container mx-auto">
-                <div class="flex">
-                    <div class="flex-1">
-                        <div>
+        <div className="my-16 hidden md:block" style={{backgroundColor: "#dd2400"}}>
+            <div className="relative container mx-auto">
+                <div className="flex">
+                    <div className="flex-1">
+                        <div className="absolute -top-16">
                             <img src={bannerImg} alt="Banner Title"/>
                         </div>
                     </div>
-                    <div class="flex-1">
-                        <div>
-                            <h2>Best Smart LED TV</h2>
-                            <p>Collection 2021</p>
-                            <p>Term & Condition May Applied</p>
+                    <div className="flex-1">
+                        <div className="text-center py-16">
+                            <h2 className="font-bold text-3xl md:text-3xl lg:text-4xl font-heading text-white">Best Smart LED TV</h2>
+                            <p className="font-thin text-3xl md:text-3xl lg:text-4xl font-heading text-white">Collection 2021</p>
+                            <p className="text-white">Term & Condition May Applied</p>
                         </div>
                     </div>
-                    <div class="flex-1">
-                    <button class="bg-custom hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
+                    <div className="flex-1">
+                        <div className="text-center py-16">
+                        <Link to="/shop" className="bg-white text-gray-700 font-bold py-4 px-8 rounded">Shop Now</Link>
+                        </div>
                     </div>
                 </div>
             </div>
