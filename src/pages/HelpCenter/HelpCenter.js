2ef00a4431d5ec9900de719d50a960e8f1a4437e
Zuned Ahmed <63390198+zunead@users.noreply.github.com>
2021-11-03 23:57:56 +0600

    dynamic contact message

--- src/pages/HelpCenter/HelpCenter.js
+++ src/pages/HelpCenter/HelpCenter.js
@@ -19,52 +19,6 @@ const HelpCenter = () => {
           </div>
           <div class="container mx-auto pb-12">
             <div class="flex flex-wrap">
-<<<<<<< HEAD
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/how-to-buy">How to buy a product from mamar dukan.</Link> </h2>
-                </div>
-              </div>
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/return-and-refund">Mamar dukan return & refund policy</Link></h2>
-                </div>
-              </div>
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/affiliates">Mamar dukan affiliate policy</Link> </h2>
-                </div>
-              </div>
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/terms-and-conditions">Mamar dukan terms & conditions</Link> </h2>
-                </div>
-              </div>
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/privacy-policy">Mamar dukan privacy policy</Link> </h2>
-                </div>
-              </div>              
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/trackOrder">How to your track order</Link> </h2>
-                </div>
-              </div>
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/privacy-policy">Mamar dukan cookies policy</Link> </h2>
-                </div>
-              </div>
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/contact">Mamar dukan customer care</Link> </h2>
-                </div>
-              </div>
-              <div class="xl:w-1/3 md:w-1/2 p-4">
-                <div class="shadow-md border border-gray-300 hover:border-red-600 px-6 py-4 rounded-lg">
-                  <h2 class="text-lg  font-medium title-font mb-2"><Link to="/blog">Mamar dukan blogs</Link> </h2>
-                </div>
-=======
               <div class=" xl:w-1/3 md:w-1/2 p-4">
                 <Link to="/how-to-buy"><div class="cursor-pointer hover:bg-red-500  border border-gray-300 px-6 py-4 rounded-lg">
                   <h2 class=" text-lg  font-medium title-font mb-2">How to buy a product from mamar dukan. </h2>
@@ -109,7 +63,6 @@ const HelpCenter = () => {
                 <Link to="/blog"><div class="cursor-pointer hover:bg-red-500  border border-gray-300 px-6 py-4 rounded-lg">
                   <h2 class="text-lg  font-medium title-font mb-2">Mamar dukan blogs </h2>
                 </div></Link>
->>>>>>> 96592d1c825b0e0f23f0908287dba6c757c50e7b
               </div>
             </div>
           </div>
